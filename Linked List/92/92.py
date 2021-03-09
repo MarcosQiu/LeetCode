5cd2c0aa7655a4b321dd406e4f2ae9f1d79dbc5e
Yankun Qiu <yqiu@freelancer.com>
2021-03-09 23:01:49 +1100

    clean up

--- Linked List/92/92.py
+++
@@ -1,7 +0,0 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
